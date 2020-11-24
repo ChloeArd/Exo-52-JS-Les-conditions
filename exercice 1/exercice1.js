e5475efcd77dbc1e44c7004aca4ae81692e25e74
ChloeArd <chloe.ardoise@gmail.com>
2020-11-24 14:20:49 +0100

    Exercice 52

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,6 +1,13 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-if x > y
-    document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+let x = 2;
+let y = 1;
 
+if (x < y) {
+    document.getElementById('monDiv').innerHTML = "La variable x est inférieur à la variable y";
+}
+
+else {
+    document.getElementById('monDiv').innerHTML = "La variable x est supérieur à la variable y";
+}
 
--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,16 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-if()
-{
+let x = 2;
+let y = 2;
 
+if( x === y) {
+    document.getElementById('monDiv').innerHTML = "La variable x est strictement égale à la variable y";
 }
 
-{
+else{
+    document.getElementById('monDiv').innerHTML = "La variable x n'est pas strictement égale à la variable y";
+}
 
+if (x = 3++){
+    document.getElementById('monDiv').innerHTML = "La variable x + 3 ";
 }
